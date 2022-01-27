2addeca4aa128e7ac178c0e64cae54feec40ae37
adrancarnavale <adran.carnavale@gmail.com>
2022-01-27 18:59:18 -0300

    finishes requirement 09

---
+++ src/Components/FoodRecipes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderMainScreen from './HeaderMainScreen';
+
+function FoodRecipes() {
+  return (
+    <div>
+      <HeaderMainScreen />
+    </div>
+  );
+}
+
+export default FoodRecipes;
