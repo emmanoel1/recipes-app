fe1d7bf3a09609dce68c8180f9a7babe681af7af
Filipe Ferreira Paulo <filipef.paulo@gmail.com>
2022-01-28 17:26:23 -0300

    feat: requisitos 25, 26 e 27 feitos

--- src/pages/DrinkRecipes.js
+++ src/pages/DrinkRecipes.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getDrinkRecipes } from '../services';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Card from '../components/CardComponents/Card';
+
+import '../css/MainContainerRecipes.css';
+
+const RECIPES = 12;
 
 function DrinkRecipes() {
+  const [drinks, setDrinks] = useState([]);
+  useEffect(() => {
+    getDrinkRecipes().then((e) => setDrinks(e.drinks));
+  }, []);
+
   return (
-    <div>
+    <div className="mainRecipesContainer">
       <Header
         title
         profile
         explore
         pageTitle="Drinks"
       />
-      Drinks
+      <main className="mainContent">
+        {drinks.length > 0 && drinks.map((meal, index) => (
+          index < RECIPES && (
+            <Card
+              index={ index }
+              image={ meal.strDrinkThumb }
+              key={ meal.idDrink }
+              name={ meal.strDrink }
+            />
+          )
+        ))}
+      </main>
       <Footer />
     </div>
   );
--- src/pages/FoodRecipes.js
+++ src/pages/FoodRecipes.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getFoodRecipes } from '../services';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Card from '../components/CardComponents/Card';
+
+import '../css/MainContainerRecipes.css';
+
+const RECIPES = 12;
 
 function FoodRecipes() {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    getFoodRecipes().then((e) => setMeals(e.meals));
+  }, []);
+
   return (
-    <div>
+    <div className="mainRecipesContainer">
       <Header
         title
         profile
         explore
         pageTitle="Foods"
       />
-      Foods
+      <main className="mainContent">
+        {meals.length > 0 && meals.map((meal, index) => (
+          index < RECIPES && (
+            <Card
+              index={ index }
+              image={ meal.strMealThumb }
+              key={ meal.idMeal }
+              name={ meal.strMeal }
+            />
+          )
+        ))}
+      </main>
       <Footer />
     </div>
   );
