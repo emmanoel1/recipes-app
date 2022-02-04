073a8a4cabc13517ad6094e750334df2bc7112f0
emmanoel <emmanoelporto@gmail.com>
2022-02-04 18:36:46 -0300

    fix: wrong render in checkbox after Click

--- src/components/InProgress/StepIngredients.js
+++ src/components/InProgress/StepIngredients.js
@@ -3,34 +3,10 @@ import React, { useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
 
 import '../../css/RecipeIngredients.css';
+import { useHistory } from 'react-router-dom';
 
 function StepIngredients({ ingredients, quantities }) {
   const [checkbox, setCheckbox] = useState();
-  const results = JSON.parse(localStorage.getItem('recipeProgress'));
-
-  if (results === null) {
-    const arr = [];
-    for (let i = 0; i < ingredients.length; i += 1) {
-      arr.push({
-        index: i,
-        bool: false,
-      });
-    }
-    setCheckbox(results);
-    localStorage.setItem('recipeProgress', JSON.stringify(arr));
-  }
-
-  const handleChange = ({ target }) => {
-    const index = Number(target.name);
-    const arr = checkbox;
-    if (arr !== null && arr !== undefined) {
-      arr[index].bool = !checkbox[index].bool;
-      setCheckbox(arr);
-      localStorage.setItem('recipeProgress', JSON.stringify(arr));
-    }
-    const result = JSON.parse(localStorage.getItem('recipeProgress'));
-    target.checked = result[index].bool;
-  };
 
   useEffect(() => {
     const result = JSON.parse(localStorage.getItem('recipeProgress'));
@@ -40,29 +16,58 @@ function StepIngredients({ ingredients, quantities }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const results = JSON.parse(localStorage.getItem('recipeProgress'));
+  const history = useHistory();
+
+  if (!results) {
+    const arr = [];
+    for (let i = 0; i < ingredients.length; i += 1) {
+      arr.push({
+        index: i,
+        bool: false,
+      });
+    }
+    localStorage.setItem('recipeProgress', JSON.stringify(arr));
+  }
+
+  function handleChange({ target }) {
+    const index = Number(target.name);
+    const urlFilter = window.location.pathname.split('/');
+    if (checkbox !== null && checkbox !== undefined) {
+      const arr = checkbox;
+      arr[index].bool = !checkbox[index].bool;
+      setCheckbox(arr);
+      localStorage.setItem('recipeProgress', JSON.stringify(arr));
+    }
+    history.push(`/${urlFilter[1]}/${urlFilter[2]}/${urlFilter[3]}`);
+  }
+
   return (
     <div>
-      {results !== null && checkbox !== undefined && checkbox !== null ? (
-        <div className="ingredients-container">
-          <h1 className="ingredients-header">Ingredients</h1>
-          {ingredients.map((ingredient, index) => (
-            <label
-              data-testid={ `${index}-ingredient-step` }
-              key={ index }
-              htmlFor={ ingredient }
-              className="progress-item-element"
-            >
-              <input
-                type="checkbox"
-                id={ ingredient }
-                name={ index }
-                checked={ results[index].bool }
-                onChange={ handleChange }
-              />
-              {`${ingredient} - ${quantities[index]}`}
-            </label>
-          ))}
-        </div>) : (<p>loading</p>)}
+      {checkbox !== null
+      && checkbox !== undefined
+        ? (
+          <div className="ingredients-container">
+            <h1 className="ingredients-header">Ingredients</h1>
+            {ingredients.map((ingredient, index) => (
+              <label
+                data-testid={ `${index}-ingredient-step` }
+                key={ index }
+                htmlFor={ ingredient }
+                className="progress-item-element"
+              >
+                <input
+                  type="checkbox"
+                  id={ ingredient }
+                  name={ index }
+                  checked={ checkbox[index].bool }
+                  onChange={ handleChange }
+                />
+                {`${ingredient} - ${quantities[index]}`}
+              </label>
+            ))}
+          </div>
+        ) : (<p>loading</p>)}
     </div>
   );
 }
