d67eed768ac557192fcc6afd4b50aafcbe045f6e
adrancarnavale <adran.carnavale@gmail.com>
2022-02-07 16:15:55 -0300

    deletes mock from component

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -7,37 +7,14 @@ import DoneRecipesCards from '../components/DoneRecipes/DoneRecipesCards';
 import '../css/MainContainerRecipes.css';
 
 function DoneRecipes() {
-  const doneFoods = [
-    {
-      id: '52771',
-      type: 'food',
-      nationality: 'Italian',
-      category: 'Vegetarian',
-      alcoholicOrNot: '',
-      name: 'Spicy Arrabiata Penne',
-      image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      doneDate: '23/06/2020',
-      tags: ['Pasta', 'Curry'],
-    },
-    {
-      id: '178319',
-      type: 'drink',
-      nationality: '',
-      category: 'Cocktail',
-      alcoholicOrNot: 'Alcoholic',
-      name: 'Aquamarine',
-      image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      doneDate: '23/06/2020',
-      tags: [],
-    },
-  ];
+  // test
 
   const [doneRecipes, setDoneRecipes] = useState([]);
   const [filteredDoneRecipes, setFilteredDoneRecipes] = useState([]);
 
   useEffect(() => {
     setDoneRecipes(doneFoods);
-    setFilteredDoneRecipes(doneFoods); // setFilteredDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
+    setFilteredDoneRecipes(JSON.parse(localStorage.getItem('doneRecipes')));
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
